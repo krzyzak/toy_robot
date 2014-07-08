8f07155919f98d5d142e99a0ccb5723026e60f3e
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2014-07-08 10:37:58 +0200

    Added ability to turn left/right

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,4 +1,27 @@
 module ToyRobot
   class Robot
+    attr_reader :position, :board
+    attr_accessor :direction
+
+    DIRECTIONS = [:north, :west, :south, :east]
+
+    def initialize(position:, direction:, board:)
+      @position = position
+      @direction = direction
+      @board = board
+    end
+
+    def left!
+      @direction = DIRECTIONS[direction_index+1] || DIRECTIONS[0]
+    end
+
+    def right!
+      @direction = DIRECTIONS[direction_index-1]
+    end
+
+    private
+    def direction_index
+      DIRECTIONS.index(@direction)
+    end
   end
 end
--- test/robot_test.rb
+++ test/robot_test.rb
@@ -1,2 +1,65 @@
 class RobotTest < MiniTest::Test
+  def setup
+    @robot = ToyRobot::Robot.new(position: ToyRobot::Position.new(x: 0, y: 0), direction: :north, board: ToyRobot::Board.new(5, 5))
+  end
+
+  # left!
+
+  def test_turning_left_from_north_position_should_set_west_direction
+    @robot.direction = :north
+    @robot.left!
+
+    assert_equal :west, @robot.direction
+  end
+
+  def test_turning_left_from_west_position_should_set_south_direction
+    @robot.direction = :west
+    @robot.left!
+
+    assert_equal :south, @robot.direction
+  end
+
+  def test_turning_left_from_south_position_should_set_east_direction
+    @robot.direction = :south
+    @robot.left!
+
+    assert_equal :east, @robot.direction
+  end
+
+  def test_turning_left_from_east_position_should_set_noth_direction
+    @robot.direction = :east
+    @robot.left!
+
+    assert_equal :north, @robot.direction
+  end
+
+  # right!
+
+  def test_turning_right_from_north_position_should_set_east_direction
+    @robot.direction = :north
+    @robot.right!
+
+    assert_equal :east, @robot.direction
+  end
+
+  def test_turning_right_from_east_position_should_set_south_direction
+    @robot.direction = :east
+    @robot.right!
+
+    assert_equal :south, @robot.direction
+  end
+
+  def test_turning_right_from_south_position_should_set_west_direction
+    @robot.direction = :south
+    @robot.right!
+
+    assert_equal :west, @robot.direction
+  end
+
+  def test_turning_right_from_west_position_should_set_noth_direction
+    @robot.direction = :west
+    @robot.right!
+
+    assert_equal :north, @robot.direction
+  end
 end
