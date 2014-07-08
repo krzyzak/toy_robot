b4ced2d40702ff3d1373aa07f50dee047dfdcda1
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2014-07-08 15:24:02 +0200

    Refactor Board class to use Struct

--- lib/toy_robot/board.rb
+++ lib/toy_robot/board.rb
@@ -1,12 +1,5 @@
 module ToyRobot
-  class Board
-    attr_reader :x, :y
-
-    def initialize(x, y)
-      @x = x
-      @y = y
-    end
-
+  class Board < Struct.new(:x, :y)
     def valid?(position)
       (0..x).cover?(position.x) && (0..y).cover?(position.y)
     end
